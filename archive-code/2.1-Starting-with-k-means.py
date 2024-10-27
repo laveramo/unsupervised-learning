44d4ef51613b500fef4b46f967e9479b8ebf15dc
luz <laveramo@unal.edu.co>
2024-10-27 17:48:25 +0100

    k-means corrections

--- archive-code/2.1-Starting-with-k-means.py
+++ archive-code/2.1-Starting-with-k-means.py
@@ -18,7 +18,7 @@ from sklearn import metrics
 # Exemple :  k-Means Clustering
 
 path = './artificial/'
-name="2sp2glob.arff"
+name="xclara.arff"
 
 #path_out = './fig/'
 databrut = arff.loadarff(open(path+str(name), 'r'))
@@ -40,39 +40,31 @@ plt.title("Donnees initiales : "+ str(name))
 #plt.savefig(path_out+"Plot-kmeans-code1-"+str(name)+"-init.jpg",bbox_inches='tight', pad_inches=0.1)
 plt.show()
 
-inerties = []
-clusters = range(2,10)
 # Run clustering method for a given number of clusters
 print("------------------------------------------------------")
-for k in clusters:
-    print("Appel KMeans pour une valeur de k fixée")
-    tps1 = time.time()
-    model = cluster.KMeans(n_clusters=k, init='k-means++', n_init=1)
-    model.fit(datanp)
-    tps2 = time.time()
-    labels = model.labels_
-    # informations sur le clustering obtenu
-    iteration = model.n_iter_
-    inertie = model.inertia_
-    centroids = model.cluster_centers_
-    inerties.append(inertie)
+print("Appel KMeans pour une valeur de k fixée")
+tps1 = time.time()
+k=3
+model = cluster.KMeans(n_clusters=k, init='k-means++', n_init=1)
+model.fit(datanp)
+tps2 = time.time()
+labels = model.labels_
+# informations sur le clustering obtenu
+iteration = model.n_iter_
+inertie = model.inertia_
+centroids = model.cluster_centers_
 
-    #plt.figure(figsize=(6, 6))
-    #plt.scatter(f0, f1, c=labels, s=8)
-    #plt.scatter(centroids[:, 0],centroids[:, 1], marker="x", s=50, linewidths=3, color="red")
-    #plt.title("Données après clustering : "+ str(name) + " - Nb clusters ="+ str(k))
-    #plt.savefig(path_out+"Plot-kmeans-code1-"+str(name)+"-cluster.jpg",bbox_inches='tight', pad_inches=0.1)
-    #plt.show()
-
-    print("nb clusters =",k,", nb iter =",iteration, ", inertie = ",inertie, ", runtime = ", round((tps2 - tps1)*1000,2),"ms")
-    #print("labels", labels)
-
-# from sklearn.metrics.pairwise import euclidean_distances
-# dists = euclidean_distances(centroids)
-# print(dists)
-plt.plot(clusters, inerties, "b:o")
-plt.title("Inertie vs clusters")
-plt.xlabel("# clusters")
-plt.ylabel("Inertie")
+#plt.figure(figsize=(6, 6))
+plt.scatter(f0, f1, c=labels, s=8)
+plt.scatter(centroids[:, 0],centroids[:, 1], marker="x", s=50, linewidths=3, color="red")
+plt.title("Données après clustering : "+ str(name) + " - Nb clusters ="+ str(k))
+#plt.savefig(path_out+"Plot-kmeans-code1-"+str(name)+"-cluster.jpg",bbox_inches='tight', pad_inches=0.1)
 plt.show()
+
+print("nb clusters =",k,", nb iter =",iteration, ", inertie = ",inertie, ", runtime = ", round((tps2 - tps1)*1000,2),"ms")
+#print("labels", labels)
+
+from sklearn.metrics.pairwise import euclidean_distances
+dists = euclidean_distances(centroids)
+print(dists)
 
--- archive-code/2.2-2.3-k-means-inertie-silhouette.py
+++ archive-code/2.2-2.3-k-means-inertie-silhouette.py
@@ -1,8 +1,3 @@
-"""
-Created on 2023/09/11
-
-@author: huguet
-"""
 import os
 os.environ["OMP_NUM_THREADS"] = '4'
 
@@ -20,15 +15,9 @@ from sklearn.metrics import silhouette_score
 path = './artificial/'
 name="2sp2glob.arff"
 
-#path_out = './fig/'
 databrut = arff.loadarff(open(path+str(name), 'r'))
 datanp = np.array([[x[0],x[1]] for x in databrut[0]])
 
-# PLOT datanp (en 2D) - / scatter plot
-# Extraire chaque valeur de features pour en faire une liste
-# EX : 
-# - pour t1=t[:,0] --> [1, 3, 5, 7]
-# - pour t2=t[:,1] --> [2, 4, 6, 8]
 print("---------------------------------------")
 print("Affichage données initiales            "+ str(name))
 f0 = datanp[:,0] # tous les élements de la première colonne
@@ -37,11 +26,12 @@ f1 = datanp[:,1] # tous les éléments de la deuxième colonne
 #plt.figure(figsize=(6, 6))
 plt.scatter(f0, f1, s=8)
 plt.title("Donnees initiales : "+ str(name))
-#plt.savefig(path_out+"Plot-kmeans-code1-"+str(name)+"-init.jpg",bbox_inches='tight', pad_inches=0.1)
 plt.show()
 
+inerties = []
 silhouette_scores = []
 clusters = range(2,10)
+
 # Run clustering method for a given number of clusters
 print("------------------------------------------------------")
 for k in clusters:
@@ -53,25 +43,23 @@ for k in clusters:
     labels = model.labels_
     # informations sur le clustering obtenu
     iteration = model.n_iter_
+    inertie = model.inertia_
     centroids = model.cluster_centers_
+    inerties.append(inertie)    
     silhouette_avg = silhouette_score(datanp, labels)
     silhouette_scores.append(silhouette_avg)
-    #plt.figure(figsize=(6, 6))
-    #plt.scatter(f0, f1, c=labels, s=8)
-    #plt.scatter(centroids[:, 0],centroids[:, 1], marker="x", s=50, linewidths=3, color="red")
-    #plt.title("Données après clustering : "+ str(name) + " - Nb clusters ="+ str(k))
-    #plt.savefig(path_out+"Plot-kmeans-code1-"+str(name)+"-cluster.jpg",bbox_inches='tight', pad_inches=0.1)
-    #plt.show()
 
-    print("nb clusters =",k,", nb iter =",iteration, ", silhouette score = ",silhouette_avg, ", runtime = ", round((tps2 - tps1)*1000,2),"ms")
-    #print("labels", labels)
+    print("nb clusters =",k,", nb iter =",iteration, ", silhouette score = ",silhouette_avg, ", inertie = ",inertie, ", runtime = ", round((tps2 - tps1)*1000,2),"ms")
 
-# from sklearn.metrics.pairwise import euclidean_distances
-# dists = euclidean_distances(centroids)
-# print(dists)
+
+plt.plot(clusters, inerties, "b:o")
+plt.title("Inertie vs clusters")
+plt.xlabel("# clusters")
+plt.ylabel("Inertie")
+plt.show()
+
 plt.plot(clusters, silhouette_scores, "b:o")
 plt.title("Silhouette value vs clusters")
 plt.xlabel("# clusters")
 plt.ylabel("Silhouette value")
 plt.show()
-
